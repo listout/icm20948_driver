221029722853d88f20983a3231180d9d713b443c
listout <listout@protonmail.com>
2022-10-15 12:48:23 +0530

    initial commit
    Signed-off-by: listout <listout@protonmail.com>

---
+++ .vimrc.lua
@@ -0,0 +1,13 @@
+vim.opt.autochdir = true
+require('lspconfig')['clangd'].setup {
+	cmd = { "/home/listout/Documents/w/xtensa-clang/bin/clangd" },
+	on_attach = require("plugins.lsp-config").on_attach,
+	cpabilities = require("plugins.lsp-config").cpabilities,
+	lsp_flags = require("plugins.lsp-config").lsp_flags,
+}
+require('lspconfig')['cmake'].setup {
+	cmd = { "/home/listout/.local/bin/cmake-language-server" },
+	on_attach = require("plugins.lsp-config").on_attach,
+	cpabilities = require("plugins.lsp-config").cpabilities,
+	lsp_flags = require("plugins.lsp-config").lsp_flags,
+}
